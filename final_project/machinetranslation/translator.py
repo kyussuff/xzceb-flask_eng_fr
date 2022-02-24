1b23c03e0f3039590e414f6cdc7d2014c35021f7
kyussuff <khadijat.yussuff@gmail.com>
2022-02-24 06:41:20 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,16 +9,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-apikey = os.environ['1ZMEz1Ao6_0c01LmmcQC3V5gkwgM3NlEJGWk2Zg3LAQB']
-url = os.environ['https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/59b330bd-954c-4c3a-bbc4-7c4663ed44c0']
+apikey = os.environ['apikey']
+url = os.environ['url']
 
 authenticator = IAMAuthenticator('1ZMEz1Ao6_0c01LmmcQC3V5gkwgM3NlEJGWk2Zg3LAQB')
 language_translator = LanguageTranslatorV3(
-    version='5.3.1',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
+language_translator.set_service_url('https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/59b330bd-954c-4c3a-bbc4-7c4663ed44c0')
 
 def english_to_french(english_text):
     if english_text == "" or english_text is None:
